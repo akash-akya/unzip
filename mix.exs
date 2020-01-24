555d598f1a4e642375383e5b28e812e5f2134f79
akash-akya <akashh246@gmail.com>
2020-01-24 23:07:15 +0530

    Publish package

--- mix.exs
+++ mix.exs
@@ -7,22 +7,40 @@ defmodule Unzip.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Package
+      package: package(),
+      description: description(),
+
+      # Docs
+      source_url: "https://github.com/akash-akya/unzip",
+      homepage_url: "https://github.com/akash-akya/unzip",
+      docs: [
+        main: "Unzip"
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp description do
+    "Module to get files out of a zip. Works with local and remote files"
+  end
+
+  defp package do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      maintainers: ["Akash Hiremath"],
+      licenses: ["MIT"],
+      links: %{GitHub: "https://github.com/akash-akya/unzip"}
     ]
   end
 end
